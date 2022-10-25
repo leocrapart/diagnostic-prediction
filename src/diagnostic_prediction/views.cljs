241d7943b19926460a4040a5ed0a4f315a647931
Leo Crapart <leocrapart@yahoo.fr>
2022-10-26 00:29:07 +0200

    design document

--- src/diagnostic_prediction/views.cljs
+++ src/diagnostic_prediction/views.cljs
@@ -22,6 +22,15 @@
 ;  :y 10
 ;  :z 1}
 
+; fc: frequence cardiaque
+; c:  cianose
+; t:  tension
+
+; si t> x :
+;  diagnostics qui existent à ce sujet
+
+; si deux infos contradictoires : 
+;   enlever certains diagnostics/leur donner un score de pertinence plus faible
 
 ; diagnostic = 
 ; {:title 
@@ -29,3 +38,30 @@
 ;  :images?
 ;  :url
 ;  :pertinence}
+
+
+; diagnosis vs prognosis
+; https://pubmed.ncbi.nlm.nih.gov/33775387/
+; diagnosis = % of disease in present
+; prognosis = % of disease in futur
+
+
+
+
+
+;; roadmap
+; 5 maladies cardiaques
+; tout le coeur
+; les poumons
+; ...
+; tous les organes
+
+
+
+;; client 
+; landes, milieu reculé sans médecins
+; logiciel validé par le samu
+; médecin seul apte à déclarer le décès
+; infirmières à domicile
+; appeler le samu ou pas
+; urgence urgence ou ambulance demain hopital
